be0b8b8463d2cbaf3bec2ab45cd981dd3076bd3f
faux123 <reioux@gmail.com>
2013-05-09 17:30:45 -0700

    CPU Under/Overclock 162-2106 MHz [Squash]
    Low CPU Clocks: Allow ultra low CPU frequencies for better battery performance
    
    while screen is on or background operations like audio books and music
    
    Signed-off-by: faux123 <reioux@gmail.com>
    
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8064.c
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    
    cpufreq: allow for low CPU clock policy when hotplugging new cores
    
    Signed-off-by: faux123 <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c
    
    CPU Overclock: initial overclock for Krait 600 (2.0 GHz variants)
    
    overclocking up to 2.3 GHz
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8064.c
    	arch/arm/mach-msm/board-jf_att.c
    	arch/arm/mach-msm/board-jf_eur.c
    	arch/arm/mach-msm/board-jf_spr.c
    	arch/arm/mach-msm/board-jf_tmo.c
    
    krait-defines: create a new common header file shared used by various drivers
    
    code to help organize common MACRO definitions into 1 file
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    overclocking: make 2.1+ GHz a separate compilation switch
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8064.c
    
    CPU Overclocking: remove the highest 2.3 GHz freq due to stability issues
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    
    Have hotplugging consider OC freqs
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/board-8064-regulator.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -393,6 +393,15 @@ int msm_cpufreq_set_freq_limits(uint32_t cpu, uint32_t min, uint32_t max)
 }
 EXPORT_SYMBOL(msm_cpufreq_set_freq_limits);
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define LOW_CPUCLOCKS_FREQ_MIN	162000
+#endif
+
+#ifdef CONFIG_CPU_OVERCLOCK
+#define HIGH_CPUCLOCKS_FREQ_MAX  2106000
+#endif
+
+
 static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 {
 	int cur_freq;
@@ -414,13 +423,29 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
+#ifdef CONFIG_LOW_CPUCLOCKS
+		policy->cpuinfo.min_freq = LOW_CPUCLOCKS_FREQ_MIN;
+#else
 		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
+#endif
+#ifdef CONFIG_CPU_OVERCLOCK
+		policy->cpuinfo.max_freq = HIGH_CPUCLOCKS_FREQ_MAX;
+#else
 		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
+#endif
 #endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
+#ifdef CONFIG_LOW_CPUCLOCKS
+	policy->min = LOW_CPUCLOCKS_FREQ_MIN;
+#else
 	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
+#endif
+#ifdef CONFIG_CPU_OVERCLOCK
+	policy->max = HIGH_CPUCLOCKS_FREQ_MAX;
+#else
 	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+#endif
 #endif
 
 #ifdef CONFIG_SEC_DVFS
