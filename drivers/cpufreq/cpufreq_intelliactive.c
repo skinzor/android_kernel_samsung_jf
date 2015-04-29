05ce4207fdfd5281e342c4238dd45e45f402b669
xsynergy510x <xsynergy510x@users.noreply.github.com>
2015-04-29 07:04:27 -0700

    Update cpufreq_intelliactive.c

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -65,7 +65,7 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
-static unsigned int hispeed_freq = 1574400;
+static unsigned int hispeed_freq = 1566000;
 
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 99
@@ -75,7 +75,7 @@ static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 static unsigned int sampling_down_factor = 1;
 
 /* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 85
+#define DEFAULT_TARGET_LOAD 90
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 static spinlock_t target_loads_lock;
 static unsigned int *target_loads = default_target_loads;
@@ -130,10 +130,10 @@ static bool io_is_busy = 1;
  * sync_freq
  */
 static unsigned int up_threshold_any_cpu_load = 95;
-static unsigned int sync_freq = 1026000;
-static unsigned int up_threshold_any_cpu_freq = 1350000;
+static unsigned int sync_freq = 702000;
+static unsigned int up_threshold_any_cpu_freq = 1026000;
 
-static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1566000} ;
+static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1350000} ;
 
 static int cpufreq_governor_intelliactive(struct cpufreq_policy *policy,
         unsigned int event);
