af0b25fadaf00a3112bec062512011639438e80b
Eduard Alfonso <xsynergy510x@gmail.com>
2015-10-20 09:15:35 -0700

    Tune adaptive LMK based on Dorimanx kernel source: https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/a66fd997647fe35be2959a6b49ba5bf8b013445e

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -142,14 +142,13 @@ static bool avoid_to_kill(uid_t uid)
 	 * uid == 1001 > radio
 	 * uid == 1002 > bluetooth
 	 * uid == 1010 > wifi
-	 * uid == 1013 > media
 	 * uid == 1014 > dhcp
 	 * uid == 1021 > gps
 	 * uid == 1027 > nfc
 	 */
 	if (uid == 1001 || uid == 1002 || uid == 1010
-			|| uid == 1013 || uid == 1014
-			|| uid == 1021 || uid == 1027)
+			|| uid == 1014 || uid == 1021 
+			|| uid == 1027)
 		return 1;
 	return 0;
 }
@@ -176,12 +175,12 @@ module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
 
 /*
  * This parameter controls the behaviour of LMK when vmpressure is in
- * the range of 90-94. Adaptive lmk triggers based on number of file
+ * the range of 88-94. Adaptive lmk triggers based on number of file
  * pages wrt vmpressure_file_min, when vmpressure is in the range of
- * 90-94. Usually this is a pseudo minfree value, higher than the
+ * 85-94. Usually this is a pseudo minfree value, higher than the
  * highest configured value in minfree array.
  */
-static int vmpressure_file_min;
+static int vmpressure_file_min = 66560; /* (65 * 1024) * 4 = 266 MB */
 module_param_named(vmpressure_file_min, vmpressure_file_min, int,
 	S_IRUGO | S_IWUSR);
 
@@ -229,7 +228,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 
 		atomic_set(&shift_adj, 1);
 		trace_almk_vmpressure(pressure, other_free, other_file);
-	} else if (pressure >= 90) {
+	} else if (pressure >= 85) {
 		if (lowmem_adj_size < array_size)
 			array_size = lowmem_adj_size;
 		if (lowmem_minfree_size < array_size)
@@ -663,7 +662,7 @@ static int android_oom_handler(struct notifier_block *nb,
 #else
 		if (avoid_to_kill(uid)) {
 #endif
-			if (tasksize * (long)(PAGE_SIZE / 1024) >= 80000) {
+			if (tasksize * (long)(PAGE_SIZE / 1024) >= 50000) {
 				selected = p;
 				selected_tasksize = tasksize;
 				selected_oom_score_adj = oom_score_adj;
