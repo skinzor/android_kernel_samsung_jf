f7d0fa3d69c03592f8808c0a06a393c0a575b523
Eduard Alfonso <xsynergy510x@gmail.com>
2016-01-09 15:42:30 -0800

    Sync ZRAM with hammerhead-caf

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -68,12 +68,7 @@ static ssize_t disksize_store(struct device *dev,
 		pr_info("Cannot change disksize for initialized device\n");
 		return -EBUSY;
 	}
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-	if (!disksize) {
-		disksize = default_disksize_perc_ram *
-					((totalram_pages << PAGE_SHIFT) / 100);
-	}
-#endif
+
 	zram->disksize = PAGE_ALIGN(disksize);
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
 	up_write(&zram->init_lock);
@@ -100,27 +95,20 @@ static ssize_t reset_store(struct device *dev,
 	zram = dev_to_zram(dev);
 	bdev = bdget_disk(zram->disk, 0);
 
-	if (!bdev)
-		return -ENOMEM;
-
 	/* Do not reset an active device! */
-	if (bdev->bd_holders) {
-		ret = -EBUSY;
-		goto out;
-	}
+	if (bdev->bd_holders)
+		return -EBUSY;
 
 	ret = kstrtou16(buf, 10, &do_reset);
 	if (ret)
-		goto out;
+		return ret;
 
-	if (!do_reset) {
-		ret = -EINVAL;
-		goto out;
-	}
+	if (!do_reset)
+		return -EINVAL;
 
 	/* Make sure all pending I/O is finished */
-	fsync_bdev(bdev);
-	bdput(bdev);
+	if (bdev)
+		fsync_bdev(bdev);
 
 	down_write(&zram->init_lock);
 	if (zram->init_done)
@@ -128,10 +116,6 @@ static ssize_t reset_store(struct device *dev,
 	up_write(&zram->init_lock);
 
 	return len;
-
-out:
-	bdput(bdev);
-	return ret;
 }
 
 static ssize_t num_reads_show(struct device *dev,
@@ -202,8 +186,10 @@ static ssize_t mem_used_total_show(struct device *dev,
 	u64 val = 0;
 	struct zram *zram = dev_to_zram(dev);
 
-	if (zram->init_done)
-		val = zs_get_total_size_bytes(zram->mem_pool);
+	if (zram->init_done) {
+		val = zs_get_total_size_bytes(zram->mem_pool) +
+			((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
+	}
 
 	return sprintf(buf, "%llu\n", val);
 }
